4800c332d779dbff796df850855dbb8928bfce73
Eric <owarmermusic@gmail.com>
2017-12-24 22:38:32 +0900

    start on chap 12 exercises

---
+++ c12cx_maybe.hs
@@ -0,0 +1,33 @@
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust (Just x) = True
+
+isNothing :: Maybe a -> Bool
+isNothing (Just _) = False
+isNothing Nothing = True
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee acc f Nothing = acc
+mayybee acc f (Just x) = f x
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe fb Nothing = fb
+fromMaybe fb (Just x) = x
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:xs) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (x:xs) =
+  case x of
+    Nothing -> catMaybes xs
+    Just x -> x : (catMaybes xs)
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe [] = Just []
