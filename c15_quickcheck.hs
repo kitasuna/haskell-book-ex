670a45fe37ba646a3c406aac619f2e763d9ad397
Eric Jelliffe <onwarmermusic@gmail.com>
2018-02-03 11:04:06 +0900

    working on c15

--- c15_quickcheck.hs
+++ c15_quickcheck.hs
@@ -5,3 +5,13 @@ monoidAssoc :: (Eq m, Monoid m)
             => m -> m -> m -> Bool
 monoidAssoc a b c =
   (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m)
+                   => m
+                   -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m)
+                    => m
+                    -> Bool
+monoidRightIdentity a = (a <> mempty) == a
