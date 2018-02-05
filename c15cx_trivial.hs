0b0130cfd3a3591be7e04d365faa764746d47bee
Eric <owarmermusic@gmail.com>
2018-02-05 21:11:12 +0900

    hey more semigroup instances

--- c15cx_trivial.hs
+++ c15cx_trivial.hs
@@ -84,8 +84,7 @@ type FourAssoc = (Four String [Int] [Bool] String)
               -> (Four String [Int] [Bool] String)
               -> Bool
 
---BoolConj
---
+-- BoolConj
 newtype BoolConj = BoolConj Bool deriving (Eq, Show)
 
 instance Semigroup BoolConj where
@@ -95,6 +94,48 @@ instance Arbitrary BoolConj where
   arbitrary = elements [(BoolConj True), (BoolConj False)]
 
 type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+-- BoolDisj
+newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+  (BoolDisj b1) <> (BoolDisj b2) = BoolDisj (b1 || b2)
+
+instance Arbitrary BoolDisj where
+  arbitrary = elements [(BoolDisj True), (BoolDisj False)]
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+-- Snd
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  Snd x <> _      = Snd x
+  Fst x <> Snd y  = Snd y
+  Fst x <> Fst y  = Fst y
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1, return (Fst a)),
+               (1, return (Snd b))]
+
+type OrAssoc = (Or Int Char) -> (Or Int Char) -> (Or Int Char) -> Bool
+   
+-- Combine
+newtype Combine a b =
+  Combine { unCombine :: (a -> b) }
+
+instance Semigroup b => Semigroup (Combine a b) where
+  --(Combine f) <> (Combine g) = Combine (\x -> f x <> g x)
+  (Combine f) <> (Combine g) = Combine (\x -> f x <> g x)
+
+-- shamelessly cribbed from dmvianna@github
+-- because the error in ghci was driving me batshit
+instance Show (Combine a b) where
+  show (Combine _ ) = "I got yer Show instance right here"
+
 
 -- Laws
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
@@ -109,3 +150,5 @@ main = do
   quickCheck (semigroupAssoc :: ThreeAssoc)
   quickCheck (semigroupAssoc :: FourAssoc)
   quickCheck (semigroupAssoc :: BoolConjAssoc)
+  quickCheck (semigroupAssoc :: BoolDisjAssoc)
+  quickCheck (semigroupAssoc :: OrAssoc)
