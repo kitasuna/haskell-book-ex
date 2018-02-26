c2c37e3b02f467afa4841e8b4d0787a0bbd9c324
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-02-26 21:04:10 +0900

    Done with functor instance

--- c16_funcInstances.hs
+++ c16_funcInstances.hs
@@ -31,7 +31,7 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
     a <- arbitrary
     return (Identity a)
 
-type IdentityFunctorId = (Identity Int) -> Bool
+type IdentityId = (Identity Int) -> Bool
 
 
 type IdentityFC = (Identity Int)
@@ -45,12 +45,15 @@ data Pair a = Pair a a deriving (Eq, Show)
 instance Functor Pair where
   fmap f (Pair a b) = Pair (f a) (f b)
 
-type PairFunctorId = (Pair Int) -> Bool
 
 instance (Arbitrary a) => Arbitrary (Pair a) where
   arbitrary = do
     a <- arbitrary
     return (Pair a a)
+
+type PairId = (Pair Int) -> Bool
+type PairFC = (Pair Int) -> IntToInt -> IntToInt -> Bool
+
 
 -- Two
 data Two a b = Two a b deriving (Eq, Show)
@@ -64,13 +67,90 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
     b <- arbitrary
     return (Two a b)
 
-type TwoFunctorId = (Two Int Int) -> Bool
+type TwoId = (Two Int Int) -> Bool
 type TwoFC = (Two Int Int) -> IntToInt -> IntToInt -> Bool
 
+-- Three
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+  Arbitrary (Three a b c) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      return (Three a b c)
+
+type ThreeId = (Three Int Int Int) -> Bool
+type ThreeFC = (Three Int Int Int) -> IntToInt -> IntToInt -> Bool
+
+-- Three'
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return (Three' x y z)
+
+type ThreePId = (Three' Int String) -> Bool
+type ThreePFC = (Three' Int Int) -> IntToInt -> IntToInt -> Bool
+
+-- Four
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four w x y z) = Four w x y (f z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+  Arbitrary (Four a b c d) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      d <- arbitrary
+      return (Four a b c d)
+
+type FourId = (Four Int Int Int Int) -> Bool
+type FourFC = (Four Int Int Int Int) -> IntToInt -> IntToInt -> Bool
+
+-- Four'
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+ fmap f (Four' a1 a2 a3 b) = Four' a1 a2 a3 (f b) 
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = do
+    a1 <- arbitrary
+    a2 <- arbitrary
+    a3 <- arbitrary
+    b  <- arbitrary
+    return (Four' a1 a2 a3 b)
+
+type FourPId = (Four' Int Int) -> Bool
+type FourPFC = (Four' Int Int) -> IntToInt -> IntToInt -> Bool
+
 main = do
-  quickCheck (functorIdentity :: IdentityFunctorId)
+  quickCheck (functorIdentity :: IdentityId)
   quickCheck (functorCompose  :: IdentityFC)
-  quickCheck (functorIdentity :: PairFunctorId)
-  quickCheck (functorIdentity :: TwoFunctorId)
+  quickCheck (functorIdentity :: PairId)
+  quickCheck (functorCompose  :: PairFC)
+  quickCheck (functorIdentity :: TwoId)
   quickCheck (functorCompose  :: TwoFC)
+  quickCheck (functorIdentity :: ThreeId)
+  quickCheck (functorCompose  :: ThreePFC)
+  quickCheck (functorIdentity :: ThreePId)
+  quickCheck (functorCompose  :: ThreePFC)
+  quickCheck (functorIdentity :: FourId)
+  quickCheck (functorCompose  :: FourFC)
+  quickCheck (functorIdentity :: FourPId)
+  quickCheck (functorCompose  :: FourPFC)
 
