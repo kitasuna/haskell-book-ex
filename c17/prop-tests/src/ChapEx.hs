377a53f2a0ad6bafa61234a48c8c15bde8e806ab
Eric <owarmermusic@gmail.com>
2018-06-23 16:19:44 +0900

    start c17 chap exercises

---
+++ c17/prop-tests/src/ChapEx.hs
@@ -0,0 +1,27 @@
+module ChapEx where
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair a b) = Pair (f a) (f b)
+
+instance Applicative Pair where
+  pure x = Pair x x
+  (<*>) (Pair f1 f2) (Pair x1 x2) = Pair (f1 x1) (f2 x2)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = do
+    a <- arbitrary
+    return (Pair a a)
+
+instance Eq a => EqProp (Pair a) where (=-=) = eq
+-- instance Eq a => EqProp (List a) where (=-=) = eq
+
+main :: IO ()
+main = do
+  putStrLn "-- applicative Pair a'"
+  quickBatch (applicative $ (undefined :: Pair (Int, Double, Char)))
