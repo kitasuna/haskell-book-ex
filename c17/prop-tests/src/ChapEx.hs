e6d99d1f2c1656ab125fe7987ba8db96923f1484
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-06-26 08:30:41 +0900

    Add more applicative instances (c17)
    Added applicative instances / checkers tests for the following:
    Three'
    Four
    Four'

--- c17/prop-tests/src/ChapEx.hs
+++ c17/prop-tests/src/ChapEx.hs
@@ -38,10 +38,99 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
 
 instance (Eq a, Eq b) => EqProp (Two a b) where (=-=) = eq
 
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b $ f c
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure = Three mempty mempty
+  (<*>) (Three ma1 mb1 f) (Three ma2 mb2 x) =
+    Three (ma1 <> ma2) (mb1 <> mb2) (f x)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+  Arbitrary (Three a b c) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      return (Three a b c)
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where (=-=) = eq
+
+
+data Three' a b = Three' a b b
+  deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' x y z) = Three' x (f y) (f z)
+
+instance (Monoid a) => Applicative (Three' a) where
+  pure x = Three' mempty x x
+  (<*>) (Three' m1 f f') (Three' m2 x x') = 
+    Three' (m1 <> m2) (f x) (f' x')
+
+instance (Arbitrary a, Arbitrary b) =>
+  Arbitrary (Three' a b) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      b' <- arbitrary
+      return (Three' a b b')
+
+instance (Eq a, Eq b) => EqProp (Three' a b) where (=-=) = eq
+
+data Four a b c d = Four a b c d
+  deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c $ f d
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+  pure x = Four mempty mempty mempty x
+  (<*>) (Four ma1 mb1 mc1 f) (Four ma2 mb2 mc2 x) =
+    Four (ma1 <> ma2) (mb1 <> mb2) (mc1 <> mc2) (f x)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+  Arbitrary (Four a b c d) where
+    arbitrary = do
+      a <- arbitrary
+      b <- arbitrary
+      c <- arbitrary
+      d <- arbitrary
+      return (Four a b c d)
+
+instance (Eq a, Eq b, Eq c, Eq d) => EqProp (Four a b c d) where (=-=) = eq
+
+data Four' a b = Four' a a a b deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' x y z b) = Four' x y z $ f b
+
+instance Monoid a => Applicative (Four' a) where
+  pure = Four' mempty mempty mempty
+  (<*>) (Four' ma1 mb1 mc1 f) (Four' ma2 mb2 mc2 x) = 
+    Four' (ma1 <> ma2) (mb1 <> mb2) (mc1 <> mc2) (f x)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Four' a a a b)
+
+instance (Eq a, Eq b) => EqProp (Four' a b) where (=-=) = eq
+
 main :: IO ()
 main = do
   putStrLn "-- applicative Pair a'"
   quickBatch (applicative $ (undefined :: Pair (Int, Double, Char)))
   putStrLn "-- applicative Two a b'"
   quickBatch (applicative $ (undefined :: Two (String, String, String) (String, String, String)))
-  
+  putStrLn "-- applicative Three a b c"
+  quickBatch (applicative $ (undefined :: Three (String, [Int], String) (String, [Int], String) (String, [Int], String)))
+  putStrLn "-- applicative Three' a b"
+  quickBatch (applicative $ (undefined :: Three ([Int], [String], [Bool]) ([Int], [String], [Bool]) ([Int], [String], [Bool])))
+  putStrLn "-- applicative Four a b c d"
+  quickBatch (applicative $ (undefined :: Four (String, String, String) (String, [Bool], String) ([Bool], String, String) ([Int], [Bool], String)))
+  putStrLn "-- applicative Four' a b"
+  quickBatch (applicative $ (undefined :: Four' (String, String, String) ([Bool], String, [Int])))
