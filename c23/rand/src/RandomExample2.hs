5547ed0db87f7a34163b41a20fa6f97f77de30c3
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-10-01 10:05:09 +0900

    Clean up c23 dir

--- c23/rand/src/RandomExample2.hs
+++ c23/rand/src/RandomExample2.hs
@@ -2,7 +2,7 @@ module RandomExample2 where
 
 import Control.Applicative (liftA3)
 import Control.Monad (replicateM)
-import Control.Monad.Trans.State
+import Control.Monad.Trans.State.Lazy
 import System.Random
 import RandomExample
 
