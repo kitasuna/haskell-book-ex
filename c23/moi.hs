03570fcdd6499ec7c5877a41e2309f4bd4cd0399
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-10-04 08:18:43 +0900

    Chapter 23 exercises, done

--- c23/moi.hs
+++ c23/moi.hs
@@ -22,9 +22,31 @@ instance Applicative (Moi s) where
         -> Moi s b
   (Moi f) <*> (Moi g) = Moi $ \x -> (fst (f x) $ fst (g x), x)
 
+
 instance Monad (Moi s) where
   return = pure
   (>>=) :: Moi s a
         -> (a -> Moi s b)
         -> Moi s b
   (Moi f) >>= g = Moi $ \x -> runMoi (g (fst (f x))) x
+
+  -- Needed this to get `modify` working
+  -- Thanks to dmvianna
+  -- https://github.com/dmvianna/haskellbook/blob/master/src/Ch23-MyState.hs
+  Moi f >> Moi g = Moi $ \s -> let (_, s') = f s
+                                     in g s'
+
+get :: Moi s s
+get = Moi $ (\x -> (x, x))
+
+put :: s -> Moi s ()
+put s = Moi $ (\x -> ((), s))
+
+exec :: Moi s a -> s -> s
+exec (Moi sa) s = snd $ sa s
+
+eval :: Moi s a -> s -> a
+eval (Moi sa) = (\x -> fst $ sa x)
+
+modify :: (s -> s) -> Moi s ()
+modify f = Moi $ (\x -> ((), f x))
