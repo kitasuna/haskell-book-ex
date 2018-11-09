2a276b05fc1d5660bd64e8fcad983f383a7f69ce
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-11-10 08:00:55 +0900

    Switch examples to readP

--- c24/learnP/app/Main.hs
+++ c24/learnP/app/Main.hs
@@ -1,16 +1,14 @@
 module Main where
 
-import LearnParsers
+import WithRP
+import Text.ParserCombinators.ReadP (readP_to_S)
+
+pNL s =
+  putStrLn ('\n' : s)
 
 main :: IO ()
 main = do
   --pNL "stop:"
   --testParse stop
-  pNL "one:"
-  testParse one
-  pNL "one':"
-  testParse one'
-  pNL "oneTwo:"
-  testParse oneTwo
-  pNL "oneTwo':"
-  testParse oneTwo'
+  pNL "one:" 
+  pNL $ show $ readP_to_S one "1"
