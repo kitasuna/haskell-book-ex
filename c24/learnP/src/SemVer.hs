eb43715f3a2a3b024c717156f01a4859b9466c18
Eric Jelliffe <5688945+kitasuna@users.noreply.github.com>
2018-12-08 08:36:50 +0900

    Finish up SemVer parser

--- c24/learnP/src/SemVer.hs
+++ c24/learnP/src/SemVer.hs
@@ -1,5 +1,6 @@
 module SemVer where
 
+import Control.Applicative
 import Text.Trifecta
 
 data NumberOrString =
@@ -17,10 +18,28 @@ data SemVer =
   SemVer Major Minor Patch Release Metadata
   deriving (Show, Eq)
 
+instance Ord SemVer where
+  compare (SemVer majorX minorX patchX _ _) (SemVer majorY minorY patchY _ _) = 
+    case compare majorX majorY of
+      EQ -> case compare minorX minorY of
+              EQ -> compare patchX patchY
+              LT -> LT
+              GT -> GT
+      LT -> LT
+      GT -> GT
+parseNoS :: Parser NumberOrString
+parseNoS = 
+  try (NOSI <$> decimal)
+  <|>
+  NOSS <$> some letter
+
 {-
+parseNoS' :: Parser NumberOrString
+parseNoS' = (decimal <|> some letter) `sepBy` (char '.')
+-}
+
 digit :: Parser Char
 digit = satisfy (\c -> c >= '0' && c <= '9')
--}
 
 parseSemVer :: Parser SemVer
 parseSemVer = do
@@ -29,4 +48,8 @@ parseSemVer = do
   minor <- decimal
   char '.'
   patch <- decimal
-  return $ SemVer major minor patch [] []
+  option ' ' (char '-')
+  rel <- option [] (parseNoS `sepBy` (char '.'))
+  option ' ' (char '+')
+  meta <- option [] (parseNoS `sepBy` (char '.'))
+  return $ SemVer major minor patch rel meta
