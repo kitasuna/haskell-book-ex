d9154a66996f827b2a194fa0aae6fbd36b26dc1f
Eric Jelliffe <onwarmermusic@gmail.com>
2021-02-17 23:02:10 +0900

    Chapter23: Add cabal files and refactor Moi instances

--- c23/RandomExample.hs
+++ c23/RandomExample.hs
@@ -1,6 +1,6 @@
 module RandomExample where
 
-import System.Random
+import           System.Random
 
 data Die =
     DieOne
@@ -12,20 +12,19 @@ data Die =
   deriving (Eq, Show)
 
 intToDie :: Int -> Die
-intToDie n =
-  case n of
-    1 -> DieOne
-    2 -> DieTwo
-    3 -> DieThree
-    4 -> DieFour
-    5 -> DieFive
-    6 -> DieSix
-    x -> error $ "intToDie got non 1-6 integer: " ++ show x
+intToDie n = case n of
+  1 -> DieOne
+  2 -> DieTwo
+  3 -> DieThree
+  4 -> DieFour
+  5 -> DieFive
+  6 -> DieSix
+  x -> error $ "intToDie got non 1-6 integer: " ++ show x
 
 rollDieThreeTimes :: (Die, Die, Die)
 rollDieThreeTimes = do
-  let s = mkStdGen 73892
+  let s        = mkStdGen 578934
       (d1, s1) = randomR (1, 6) s
       (d2, s2) = randomR (1, 6) s1
-      (d3, _)  = randomR (1, 6) s2
+      (d3, _ ) = randomR (1, 6) s2
   (intToDie d1, intToDie d2, intToDie d3)
